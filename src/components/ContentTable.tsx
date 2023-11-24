2fc6ee436b417a16a15ed92e8d242e3deebd569d
yaowenjun <yaowenjun@111.com.cn>
2023-11-25 02:13:54 +1000

    add loading

--- src/components/ContentTable.tsx
+++ src/components/ContentTable.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState} from "react";
 import TableEntry from "./TableEntry";
 import {Article} from "./TypeDefinition";
 import {Link} from 'react-router-dom'
+import { Spin } from 'antd';
 
 
 
@@ -13,6 +14,7 @@ type props = {
 export default function ContentTable({setArticle}:props){
 
     const [data, setData] = useState<Array<Article>>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
      function retriveArticles(){
          let url = "https://wenjunblog.xyz:7777/public/retrieve/articles";
@@ -23,7 +25,8 @@ export default function ContentTable({setArticle}:props){
                 'Content-Type': 'application/json'
             },
         }).then(response => response.json()).then(result => {
-            setData(result)
+            setLoading(false);
+            setData(result);
         })
     }
     useEffect(()=>{
@@ -34,6 +37,10 @@ export default function ContentTable({setArticle}:props){
             <div className={classes.content}>
                 <h1 className={classes.header}>Table of Content</h1>
                 <div className={classes.entryContainer}>
+                    <div className={classes.loadingContainer}>    
+                        <Spin spinning={loading} size="large">
+                        </Spin>
+                    </div>
                     {data.map((article) => <TableEntry key={article._id} setArticle={setArticle} article={article}></TableEntry>)}
                 </div>
                 <div className={classes.login}><Link to={"/login"}>Write Article</Link></div>
