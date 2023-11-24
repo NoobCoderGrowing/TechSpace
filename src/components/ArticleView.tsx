5a33be370e14a2505a4cf5fe1419dbf89ed39462
yaowenjun <yaowenjun@111.com.cn>
2023-11-25 04:58:22 +1000

    fix indentation issue

--- src/components/ArticleView.tsx
+++ src/components/ArticleView.tsx
@@ -2,6 +2,9 @@ import classes from './ArticleView.module.css'
 import { Article } from './TypeDefinition'
 import parse from 'html-react-parser'
 import elon_zuk from '../assets/elon-musk-mark-zuckerberg.gif'
+import 'react-quill/dist/quill.core.css' 
+import 'react-quill/dist/quill.bubble.css'
+ import 'react-quill/dist/quill.snow.css'
 
 type props = {
     article: Article | undefined
@@ -15,7 +18,9 @@ export default function ArticleView({article}: props){
             <div className={classes.container}>
                 <h1 className={classes.title}>{article.title}</h1>
                 <h5 className={classes.date}>{article.date}</h5>
-                {parse(article.content)}
+                <div className='ql-editor'>
+                    {parse(article.content)}
+                </div>
             </div>
         )
     }else{
