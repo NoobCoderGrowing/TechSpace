cfd461763b53edfff8a94c3d9d81f1063cc502c7
yaowenjun <yaowenjun@111.com.cn>
2023-12-02 16:14:57 +1000

    fix article font

--- src/components/BubbleBox.tsx
+++ src/components/BubbleBox.tsx
@@ -5,12 +5,12 @@ function BubbleBox(){
     return(
         <div className={classes.bubbleBorder}>
             <div className={classes.bubbleText}>           
-                <p>Hi, my name is Wayne. I graduated from The University of Queensland in 2022 with the degree of Master of Information Technology, and currently doing my Master of Statistical Data Science degree in Queensland University of Technology.</p>
+                <p>Hi, my name is Wayne. I am currently doing my Master of  Data Analytics program (Statistical Data Science major) in Queensland University of Technology.</p>
                 <p> 
-                I'm a full stack developer, and my technical stack is react + spring. I have 1 year industry experience with 4 months as a front-end software intern at <a href='https://appen.com/'>Appen</a> and 10 months as a java backend developer at <a href='https://www.yiyaowang.com/'>YI</a>. <a href='https://www.yiyaowang.com/'>YI</a> is a leading pharmaceutical e-commerce in China, and I'm responsible for the maintenance and development of their seach engine for both their mobile and PC applications.   
+                I'm a full stack developer, and my technical stack is React + Spring, and I have about 1 year industry experience in developing commercial vertical search engine. 
                 </p>
 
-                <p>I'm currently seeking an intership/part time job, and willing to relocate during my 2023 summer vacation. My interest includes full stack, backend, frontend and e-commerce search engine development.</p>
+                <p>I'm currently seeking an intership/part time job, and willing to relocate. My interest includes full stack, backend, frontend and search engine relevant development.</p>
             </div>
         </div>
     )
